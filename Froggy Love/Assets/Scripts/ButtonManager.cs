d397de831dfb1a8c0d3c7b352e626354c438b40a
Unknown <kunzch@gmx.de>
2018-06-09 17:45:20 +0200

    UI implemented

---
+++ Froggy Love/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void StartButtonPressed()
+    {
+
+        SceneManager.LoadScene("Game");
+
+    }
+
+    public void LeaveButtonPressed()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+
+
+    public void ExitButton()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+
+        Application.Quit();
+#endif
+
+
+    }
+
+ }
