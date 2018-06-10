048649ca5bb90a76c170c867929b3c2513c10eb7
Monika <monika.galkewitsch@hs-furtwangen.de>
2018-06-10 17:00:08 +0200

    Divine Intervention

---
+++ Froggy Love/Assets/Scripts/GodBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(Random.Range(0f, 1f) <= 0.5)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/GodPopuko");
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/FrogGod");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
