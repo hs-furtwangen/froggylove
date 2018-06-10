2f9c97189718f39a4e92d45b5a5e06642b722e08
Lukas Scheuerle <lukas.scheuerle@web.de>
2018-06-10 15:14:15 +0200

    Game Ends now & Scores display Correctly

---
+++ Froggy Love/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+
+	void Start () {
+		scoreText.text = "";
+		List<string> scoresList = new List<string>();
+		using (System.IO.StreamReader sr = new System.IO.StreamReader(Application.persistentDataPath + "/highscores.txt")){
+			string scoresline = "";
+			do{
+				scoresline = sr.ReadLine();
+				if(scoresline != null){
+					scoresList.Add(scoresline);
+				}
+
+			} while (scoresline != null);
+		}
+		for(int i = 0; (i < 5) && (i < scoresList.Count); i++){
+			scoreText.text += (i + 1) + ". " + scoresList[scoresList.Count - i - 1] + "\n";
+		}
+	}
+}
