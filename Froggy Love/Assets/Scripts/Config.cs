8057c6910b369b21870f99393ed33e07d1b943ab
Lukas Scheuerle <lukas.scheuerle@web.de>
2018-06-10 14:20:06 +0200

    Build1 done!

--- Froggy Love/Assets/Scripts/Config.cs
+++ Froggy Love/Assets/Scripts/Config.cs
@@ -2,5 +2,5 @@
 public static class Config {
     public static float TimeLimit = 300f;
     public static int maleFrogLimit = 5;
-    public static int femaleFrogLimit = 30;
+    public static int femaleFrogLimit = 15;
 }
